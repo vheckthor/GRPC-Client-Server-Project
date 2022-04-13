9d47bec49a43f75774098fb3bbe7c7da237afa95
victor Adebayo <adebayovicktor@gmail.com>
2022-04-13 04:12:55 +0100

    removed unused using statements

--- learngrpc/Services/CustomersService.cs
+++ learngrpc/Services/CustomersService.cs
@@ -1,9 +1,6 @@
 ﻿using Grpc.Core;
-using learngrpc;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace learngrpc.Services
