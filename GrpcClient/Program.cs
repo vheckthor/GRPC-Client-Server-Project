3850a518189faefe6d6f92b7898e0ca853be695d
victor Adebayo <adebayovicktor@gmail.com>
2022-04-13 04:06:50 +0100

    Add project files.

---
+++ GrpcClient/Program.cs
@@ -0,0 +1,40 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using learngrpc;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient
+{
+	class Program
+	{
+		static async Task Main(string[] args)
+		{
+			var request = new HelloRequest { Name = "Ade" };
+			var channel = GrpcChannel.ForAddress("https://localhost:5001");
+			var client = new Greeter.GreeterClient(channel);
+			var reply = await client.SayHelloAsync(request);
+			Console.WriteLine(reply.Message);
+			var customerClient = new Customer.CustomerClient(channel);
+			Console.WriteLine("Please input a number: ");
+			var input = int.Parse(Console.ReadLine());
+			var customerRequest = new CustomerLookupModel { UserId = input };
+			var response = await customerClient.GetCustomerInfoAsync(customerRequest);
+			Console.WriteLine($"{response.FirstName} {response.LastName} \n");
+			Console.WriteLine("''''''''''''''''''''");
+			Console.WriteLine("New Customers List");
+			Console.WriteLine("''''''''''''''''''''\n");
+			using (var call = customerClient.GetNewCustomers(new NewCustomerRequest()))
+			{
+				while (await call.ResponseStream.MoveNext())
+				{
+					var curentCustomer = call.ResponseStream.Current;
+					Console.WriteLine($" Name: {curentCustomer.FirstName} {curentCustomer.LastName} \n Age: {curentCustomer.Age} " +
+						$"\n IsAlive: {curentCustomer.IsAlive} \n Email Address: {curentCustomer.EmailAddress}");
+					Console.WriteLine(" \n ");
+				}
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ learngrpc/Services/CustomersService.cs
@@ -0,0 +1,80 @@
+﻿using Grpc.Core;
+using learngrpc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace learngrpc.Services
+{
+	public class CustomersService : Customer.CustomerBase
+	{
+		private readonly ILogger<CustomersService> _logger;
+
+		public CustomersService(ILogger<CustomersService> logger)
+		{
+			_logger = logger;
+		}
+
+		public override Task<CustomerModel> GetCustomerInfo(CustomerLookupModel request, ServerCallContext context)
+		{
+			CustomerModel output = new CustomerModel();
+
+			if (request.UserId == 1)
+			{
+				output.FirstName = "oreoluwa";
+				output.LastName = "Farodoye";
+			}
+			else if (request.UserId == 2)
+			{
+				output.FirstName = "Jagaban";
+				output.LastName = "Cravings";
+			}
+			else if (request.UserId == 3)
+			{
+				output.FirstName = "Angular";
+				output.LastName = "C sharper";
+			}
+			else
+			{
+				output.FirstName = "None";
+				output.LastName = "nill";
+			}
+			return Task.FromResult(output);
+		}
+
+		public override async Task GetNewCustomers(NewCustomerRequest request, IServerStreamWriter<CustomerModel> responseStream, ServerCallContext context)
+		{
+			List<CustomerModel> customers = new List<CustomerModel>
+			{
+				new CustomerModel
+				{
+					FirstName = "asdjf",
+					Age = 23,
+					LastName = "fjgsd",
+					EmailAddress = "fjfdj@gmail.com",
+					IsAlive = true
+				},
+				new CustomerModel
+				{
+					FirstName = "gtlkry",
+					Age = 20,
+					LastName = "dfgoggopswtwrt",
+					EmailAddress = "fghlkhhj@gmail.com",
+					IsAlive = true
+				},
+				new CustomerModel
+				{
+					FirstName = "bjfoert",
+					Age = 33,
+					LastName = "lergiusd",
+					EmailAddress = "gkjlg@gmail.com",
+					IsAlive = false
+				}
+			};
+
+			customers.ForEach(async(x) => await responseStream.WriteAsync(x));
+		}
+	}
+}
